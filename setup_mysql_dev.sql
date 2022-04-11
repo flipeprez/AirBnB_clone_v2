6e404f104165be0f2c946e4981b4adc84430a664
flipeprez <flipeprez@gmail.com>
2022-04-11 09:00:10 -0700

    fix duplicate user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
 -- create MySQL for setup devlopment
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER hbnb_dev
-'hbnb_dev'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' 
 IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.*
 TO 'hbnb_dev'@'localhost';
