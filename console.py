69cd14777f88f7615742d3d29a99817987a2f780
Diego Acosta <diegoacosta127@gmail.com>
2022-04-11 10:49:11 -0300

    do_create updated

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Console Module """
 import cmd
 import sys
+from tty import setraw
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -44,7 +45,7 @@ class HBNBCommand(cmd.Cmd):
         _cmd = _cls = _id = _args = ''  # initialize line elements
 
         # scan for general formating - i.e '.', '(', ')'
-        if not ('.' in line and '(' in line and ')' in line):
+        if ('.' in line and '(' in line and ')' in line):
             return line
 
         try:  # parse line left to right
@@ -73,8 +74,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
+                    if pline[0] == '{' and pline[-1] == '}'\
+                            and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -115,13 +116,23 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        attrs = args.split(" ")
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif attrs[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[attrs[0]]()
+        for i in range(1, len(attrs)):
+            my_attr = attrs[i].split("=")
+            if '\"' == my_attr[1][0] and '\"' == my_attr[1][-1]:
+                my_attr[1].strip('\"')
+                setattr(attrs[0], my_attr[0], my_attr[1])
+            elif '.' in my_attr[1]:
+                setattr(attrs[0], my_attr[0], float(my_attr[1]))
+            else:
+                setattr(attrs[0], my_attr[0], int(my_attr[1]))
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +283,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +291,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
