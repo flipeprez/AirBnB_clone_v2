767f17c7b696eab01fddccf4b9d9de82db867627
flipeprez <flipeprez@gmail.com>
2022-04-28 11:18:44 -0700

    modify echo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,9 +8,9 @@ sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/current
-sudo touch /data/web_static/releases/test/index.html
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-# echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee  ~/data/web_static/releases/test/index.html
+# sudo touch /data/web_static/releases/test/index.html
+# echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+echo -e '<html>\n<head>\n</head>\n<body>\nHolberton School\n </body>\n</html>' | sudo tee  /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/\tserver_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
