e36a7e3ec1292cddc6d8c66cb32e27952c765e42
flipeprez <flipeprez@gmail.com>
2022-04-28 11:13:47 -0700

    modify 0-setup .. add echo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-#Bash script that set up my web server
+# Bash script that set up my web server
 sudo apt update
-sudo apt install nginx -y
-sudo mkdir -p ~/data
-sudo mkdir -p ~/data/web_static
-sudo mkdir -p ~/data/web_static/releases
-sudo mkdir -p ~/data/web_static/shared
-sudo mkdir -p ~/data/web_static/releases/test/
-sudo mkdir -p ~/data/web_static/current
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee -a ~/data/web_static/releases/test/index.html
-sudo ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
-sudo chown -R ubuntu:ubuntu ~/data/
+sudo apt install nginx - y
+sudo mkdir -p /data
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/current
+sudo touch /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+# echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee  ~/data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/\tserver_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
