45a66f96d22118f78b6732367bbb99ffa03e35b9
flipeprez <flipeprez@gmail.com>
2022-05-14 23:09:03 +0000

    add ex

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,16 @@
+#!/usr/python3
+#A script that start flask web application
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def sayHelloFunc():
+    return ("Hello HBNB")
+
+@app.route('/hbnb',strict_slashes=False)
+def sayhbnb():
+    return ("HBNB")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,23 @@
+#!/usr/python3
+#A script that start flask web application
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def sayHelloFunc():
+    return ("Hello HBNB")
+
+@app.route('/hbnb',strict_slashes=False)
+def sayhbnb():
+    return ("HBNB")
+@app.route('/c/<text>',strict_slashes=False)
+def ctextfunc(text):
+    return("C" + text.replace("_"," "))
+@app.route('/python/(<text = "is cool">)',strict_slashes=False)
+def pythontextfunc(text):    
+    return(f"python + {text.replace("_", " ")"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
